971054eeb6153b355e2f55f8fe1b7b237ec9d62c
Jared Tulayan <2lyNJ.E@gmail.com>
2018-08-14 12:34:09 -0400

    Switch to gradle-node build env

---
+++ Gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+
+gulp.task('sass', function () {
+  var processors = [
+    autoprefixer,
+    cssnano
+  ];
+
+  return gulp.src('./sass/robot-pages.sass')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss(processors))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('sass:watch', function() {
+  gulp.watch('**/*.s*ss', {cwd: 'sass'}, ['sass']);
+});
